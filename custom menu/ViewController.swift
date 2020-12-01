d5fd3660a4fda89aa386e3b28a849db8f188d370
mohamed salah <mohamedsalah5060708090@gmail.com>
2020-12-01 21:58:30 +0200

    Initial Commit

---
+++ custom menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  custom menu
+//
+//  Created by MAK on 12/1/20.
+//  Copyright © 2020 MAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ custom menuTests/custom_menuTests.swift
@@ -0,0 +1,34 @@
+//
+//  custom_menuTests.swift
+//  custom menuTests
+//
+//  Created by MAK on 12/1/20.
+//  Copyright © 2020 MAK. All rights reserved.
+//
+
+import XCTest
+@testable import custom_menu
+
+class custom_menuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
